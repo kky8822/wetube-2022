9de6c28674eb333b8390ff37b781b7e3995e078c
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-03-12 00:04:55 +0900

    220311_Join

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,32 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, password_c, location } = req.body;
+  if (password != password_c) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "Confirm your password",
+    });
+  }
+
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
+  await User.create({
+    name,
+    email,
+    username,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("LogIn");
