f31be9e03653628cfd38934552feaca204112ec8
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-04-07 23:31:47 +0900

    220407_s3Upload

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,15 +1,20 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
-import FTPStorage from "multer-ftp";
+import sftpStorage from "multer-sftp";
 
-const ftpStorage = new FTPStorage({
-  basepath: "/remote/path",
-  ftp: {
+const storage = new sftpStorage({
+  sftp: {
     host: "kky8822.ipdisk.co.kr",
-    secure: true, // enables FTPS/FTP with TLS
+    port: 21,
     user: process.env.FTP_ID,
     password: process.env.FTP_PASSWD,
+  },
+  destination: function (req, file, cb) {
+    cb(null, "/tmp/my-uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
   },
 });
 
@@ -63,13 +68,13 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars",
   limits: { fileSize: 3000000 },
-  // storage: isHeroku ? s3ImageUploader : undefined,
-  storage: ftpStorage,
+  storage: isHeroku ? s3ImageUploader : undefined,
+  // storage: storage,
 });
 
 export const videoUpload = multer({
   dest: "uploads/videos",
   limits: { fileSize: 100000000 },
-  // storage: isHeroku ? s3VideoUploader : undefined,
-  storage: ftpStorage,
+  storage: isHeroku ? s3VideoUploader : undefined,
+  // storage: storage,
 });
